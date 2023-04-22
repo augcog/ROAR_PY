5cdf3a03e550b6f9880662d47dd483a4afc3bde6
Carl Gan <ganqx2001@gmail.com>
2023-04-21 18:10:21 -0700

    finish lidar sensor interface, updated syntax

--- roar_py_interface/sensors/lidar_sensor.py
+++ roar_py_interface/sensors/lidar_sensor.py
@@ -1,33 +1,31 @@
 from .sensor import RoarPySensor
 from serde import serde
 from dataclasses import dataclass
-from PIL import Image
 import numpy as np
-import typing
 import gymnasium as gym
 
 @serde
 @dataclass
 class RoarPyLiDARSensorData:
     # Received list of 4D points in shape (N, (X, Y, Z, I))
-    # where N is the # of points, 
-    # X, Y, Z is the x, y, z coordinate repectively
+    # where N is the # of points,
+    # X, Y, Z are the x, y, z coordinates respectively
     # I is the intensity of each array
-    # Each point consists of [x,y,z] coordinates 
+    # Each point consists of [x, y, z] coordinates
     # plus the intensity computed for that point
-    lidar_points_data: np.NDArray[np.float32]
+    lidar_points_data: np.ndarray
 
 class RoarPyLiDARSensor(RoarPySensor[RoarPyLiDARSensorData]):
     def __init__(
-        self, 
+        self,
         name: str,
         control_timestep: float,
     ):
         super().__init__(name, control_timestep)
-        
+
     def get_gym_observation_spec(self) -> gym.Space:
         return gym.spaces.Box(
-            low = -np.inf,
+            low=-np.inf,
             high=np.inf,
             shape=(1, 4),
             dtype=np.float32
