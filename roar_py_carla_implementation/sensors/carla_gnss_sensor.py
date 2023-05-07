ef942f59d7a85ef888e2f5b443b5d3c532f52a37
Coding Cookie <realtoiletcommander@gmail.com>
2023-05-07 14:32:21 -0700

    Code Review + Bugfix Camera Sensor control_timestep

--- roar_py_carla_implementation/sensors/carla_gnss_sensor.py
+++ roar_py_carla_implementation/sensors/carla_gnss_sensor.py
@@ -16,12 +16,76 @@ class RoarPyCarlaGNSSSensor(RoarPyGNSSSensor[RoarPyGNSSSensorData], RoarPyCarlaB
         name: str = "carla_gnss_sensor",
     ):
         assert sensor.type_id == "sensor.other.gnss", "Unsupported blueprint_id: {} for carla gnss sensor support".format(sensor.type_id)
-        RoarPyCarlaGNSSSensor.__init__(self, name, control_timestep = 0.0)
         RoarPyCarlaBase.__init__(self, carla_instance, sensor)
+        RoarPyCarlaGNSSSensor.__init__(self, name, control_timestep = 0.0)
         self.received_data : typing.Optional[RoarPyGNSSSensorData] = None
         sensor.listen(
             self.listen_callback
         )
+    
+    @property
+    def noise_altitude_bias(self) -> float:
+        return self._base_actor.noise_alt_bias
+
+    @noise_altitude_bias.setter
+    def noise_altitude_bias(self, value: float) -> None:
+        self._base_actor.noise_alt_bias = value
+
+    @property
+    def noise_altitude_std(self) -> float:
+        return self._base_actor.noise_alt_stddev
+
+    @noise_altitude_std.setter
+    def noise_altitude_std(self, value: float) -> None:
+        self._base_actor.noise_alt_stddev = value
+    
+    @property
+    def noise_latitude_bias(self) -> float:
+        return self._base_actor.noise_lat_bias
+    
+    @noise_latitude_bias.setter
+    def noise_latitude_bias(self, value: float) -> None:
+        self._base_actor.noise_lat_bias = value
+    
+    @property
+    def noise_latitude_std(self) -> float:
+        return self._base_actor.noise_lat_stddev
+    
+    @noise_latitude_std.setter
+    def noise_latitude_std(self, value: float) -> None:
+        self._base_actor.noise_lat_stddev = value
+    
+    @property
+    def noise_longitude_bias(self) -> float:
+        return self._base_actor.noise_lon_bias
+    
+    @noise_longitude_bias.setter
+    def noise_longitude_bias(self, value: float) -> None:
+        self._base_actor.noise_lon_bias = value
+
+    @property
+    def noise_longitude_std(self) -> float:
+        return self._base_actor.noise_lon_stddev
+    
+    @noise_longitude_std.setter
+    def noise_longitude_std(self, value: float) -> None:
+        self._base_actor.noise_lon_stddev = value
+
+    @property
+    def noise_seed(self) -> int:
+        return self._base_actor.noise_seed
+    
+    @noise_seed.setter
+    def noise_seed(self, value: int) -> None:
+        self._base_actor.noise_seed = value
+
+    @property
+    def control_timestep(self) -> float:
+        return self._base_actor.sensor_tick
+    
+    @control_timestep.setter
+    def control_timestep(self, value: float) -> None:
+        self._base_actor.sensor_tick = value
 
     async def receive_observation(self) -> RoarPyGNSSSensorData:
         while self.received_data is None:
