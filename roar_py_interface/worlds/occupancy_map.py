6c7715722d93cec1ba836ad1ef7907e0cadb7ee4
Carl Gan <ganqx2001@gmail.com>
2023-06-25 16:51:07 -0700

    added occupancy map world class

--- roar_py_interface/worlds/occupancy_map.py
+++ roar_py_interface/worlds/occupancy_map.py
@@ -9,33 +9,27 @@ class RoarPyOccupancyMap:
         self.size = size
         self.map = np.zeros((size, size))
 
-    def add_waypoint(self, waypoint):
+    def add_waypoint(self, waypoint) -> None:
         global_coordinate_pos, global_coordinate_neg = waypoint.line_representation
         global_coordinate_pos = global_coordinate_pos[:2].astype(int)
         global_coordinate_neg = global_coordinate_neg[:2].astype(int)
-        if global_coordinate_pos[0] < 0 \
-            or global_coordinate_pos[0] >= self.size \
-            or global_coordinate_pos[1] < 0 \
-            or global_coordinate_pos[1] >= self.size:
-            print("Waypoint out of bounds")
-        else:
+        if 0 <= global_coordinate_pos[0] < self.size and 0 <= global_coordinate_pos[1] < self.size:
             self.map[global_coordinate_pos[0], global_coordinate_pos[1]] = 1
-        if global_coordinate_neg[0] < 0 \
-            or global_coordinate_neg[0] >= self.size \
-            or global_coordinate_neg[1] < 0 \
-            or global_coordinate_neg[1] >= self.size:
-            print("Waypoint out of bounds")
         else:
+            print("Waypoint out of bounds")
+        if 0 <= global_coordinate_neg[0] < self.size and 0 <= global_coordinate_neg[1] < self.size:
             self.map[global_coordinate_neg[0], global_coordinate_neg[1]] = 1
+        else:
+            print("Waypoint out of bounds")
 
 
-    def add_waypoints(self, waypoints):
+    def add_waypoints(self, waypoints) -> None:
         for waypoint in waypoints:
             self.add_waypoint(waypoint)
 
-    def display(self):
+    def display(self) -> None:
         plt.imshow(self.map)
         plt.show()
 
-    def save(self, path):
+    def save(self, path) -> None:
         np.save(path, self.map)
