9111bae41690c094e970e94b79f99c6b2dc7d201
Carl Gan <ganqx2001@gmail.com>
2023-06-26 11:53:15 -0700

    changed occupancy map from boundary to road segment

--- roar_py_interface/worlds/occupancy_map.py
+++ roar_py_interface/worlds/occupancy_map.py
@@ -1,35 +1,29 @@
 import numpy as np
-from matplotlib import pyplot as plt
+from skimage.draw import polygon
+import matplotlib.pyplot as plt
+from typing import List
+from waypoint import RoarPyWaypoint  # Assuming RoarPyWaypoint class is in RoarPyWaypoint.py
 
-class RoarPyOccupancyMap:
-    map: np.ndarray # 2D Occupancy Map
-    size: int       # Size of the map (assume square map)
-
-    def __init__(self, size) -> None:
+class OccupancyMap:
+    def __init__(self, size):
         self.size = size
-        self.map = np.zeros((size, size))
+        self.map = np.zeros((size, size), dtype=np.uint8)
 
-    def add_waypoint(self, waypoint) -> None:
-        global_coordinate_pos, global_coordinate_neg = waypoint.line_representation
-        global_coordinate_pos = global_coordinate_pos[:2].astype(int)
-        global_coordinate_neg = global_coordinate_neg[:2].astype(int)
-        if (0 <= global_coordinate_pos[0] < self.size) and (0 <= global_coordinate_pos[1] < self.size):
-            self.map[global_coordinate_pos[0], global_coordinate_pos[1]] = 1
-        else:
-            print("Waypoint out of bounds")
-        if (0 <= global_coordinate_neg[0] < self.size) and (0 <= global_coordinate_neg[1] < self.size):
-            self.map[global_coordinate_neg[0], global_coordinate_neg[1]] = 1
-        else:
-            print("Waypoint out of bounds")
+    def add_road(self, waypoint1: RoarPyWaypoint, waypoint2: RoarPyWaypoint):
+        # Get the vertices of the polygon representing the road
+        vertices = np.vstack([waypoint1.line_representation, waypoint2.line_representation[::-1]])
+        rr, cc = polygon(vertices[:,0], vertices[:,1])
+        
+        # Ensure the coordinates are within the bounds of the map
+        rr, cc = rr.clip(0, self.size-1), cc.clip(0, self.size-1)
 
+        # Mark these points on the map as road
+        self.map[rr, cc] = 1
 
-    def add_waypoints(self, waypoints) -> None:
-        for waypoint in waypoints:
-            self.add_waypoint(waypoint)
+    def add_waypoints(self, waypoints: List[RoarPyWaypoint]):
+        for waypoint1, waypoint2 in zip(waypoints[:-1], waypoints[1:]):
+            self.add_road(waypoint1, waypoint2)
 
-    def display(self) -> None:
-        plt.imshow(self.map)
+    def display(self):
+        plt.imshow(self.map, cmap='Greys')
         plt.show()
-
-    def save(self, path) -> None:
-        np.save(path, self.map)
