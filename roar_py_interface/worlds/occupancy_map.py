066f479860b383ebe82ae3886ae684b2d1146079
Carl Gan <ganqx2001@gmail.com>
2023-06-26 11:58:57 -0700

    changed occupancy map from boundary to road segment

--- roar_py_interface/worlds/occupancy_map.py
+++ roar_py_interface/worlds/occupancy_map.py
@@ -7,15 +7,17 @@ from waypoint import RoarPyWaypoint  # Assuming RoarPyWaypoint class is in RoarP
 class OccupancyMap:
     def __init__(self, size):
         self.size = size
-        self.map = np.zeros((size, size), dtype=np.uint8)
+        self.map = np.zeros((size, size))
 
     def add_road(self, waypoint1: RoarPyWaypoint, waypoint2: RoarPyWaypoint):
         # Get the vertices of the polygon representing the road
-        vertices = np.vstack([waypoint1.line_representation, waypoint2.line_representation[::-1]])
-        rr, cc = polygon(vertices[:,0], vertices[:,1])
+        point1, point2 = waypoint1.line_representation
+        point3, point4 = waypoint2.line_representation[::-1]
+        vertices = np.vstack([point1, point2, point3, point4]).astype(int)
+        rr, cc = polygon(vertices[:, 0], vertices[:, 1])
         
         # Ensure the coordinates are within the bounds of the map
-        rr, cc = rr.clip(0, self.size-1), cc.clip(0, self.size-1)
+        rr, cc = rr.clip(0, self.size - 1), cc.clip(0, self.size - 1)
 
         # Mark these points on the map as road
         self.map[rr, cc] = 1
